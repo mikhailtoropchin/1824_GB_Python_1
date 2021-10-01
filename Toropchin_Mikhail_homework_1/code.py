74d1a4c52dee2d73db4059872167a4d48dee88ca
mikhail <toropchin.epid@yandex.ru>
2021-10-01 12:10:26 +0700

    1

--- Toropchin_Mikhail_homework_1/code.py
+++ Toropchin_Mikhail_homework_1/code.py
@@ -1,7 +1,41 @@
-def factorial(number):
-    result = 1
-    for x in range(1, number + 1):
-        result = result * x
-    print(result)
+# constant values
+minute = 60
+hour = 60 * 60
+day = hour * 24
+#----------------
+duration = 31651158
 
-factorial(5)
+if duration < minute:
+    print(f"{duration} сек")
+elif duration >= minute and duration < hour:
+    print(f"{duration // minute} мин {duration % minute} сек")
+elif duration >= hour and duration < day:
+    print(f"{duration // hour} час {(duration % hour) // minute} мин {(duration % hour) % minute} сек")
+elif duration >= day:
+    print(f"{(duration // day)} дн {(duration % day) // hour} час {(duration % hour) // minute} мин {(duration % hour) % minute} сек")
+
+
+# По примечанию
+list_of_durations = [645, 3942, 878465]
+
+for duration in list_of_durations:
+    if duration < minute:
+        print(f"{duration} сек")
+    elif duration >= minute and duration < hour:
+        print(f"{duration // minute} мин {duration % minute} сек")
+    elif duration >= hour and duration < day:
+        print(f"{duration // hour} час {(duration % hour) // minute} мин {(duration % hour) % minute} сек")
+    elif duration >= day:
+        print(
+            f"{(duration // day)} дн {(duration % day) // hour} час {(duration % hour) // minute} мин {(duration % hour) % minute} сек")
+
+
+
+
+
+
+
+
+
+
+
